5350d8c2fb4ad018db7f93d47172c33684b5a2ff
Tony Mputhia <tonymmputhia@gmail.com>
2024-09-26 17:16:42 +0300

    Create About section of portfolio website

---
+++ react-portfolio/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className='py-20 text-center'>
+      <h2 className='text-3xl font-semibold'>About Me</h2>
+      <p className="mt-4 text-lg max-w-2xl mx-auto">
+        I'm a proficient software engineer with expertise in Python, JavaScript, and C.
+        I love building applications and solving problems using these languages.
+      </p>
+    </section>
+  );
+}
+
+export default About;
